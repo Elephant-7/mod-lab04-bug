6b52786a5d8c4b73409d63125bbe4b1ddbd0a440
JJ O <drendi9n@gmail.com>
2026-05-27 13:01:38 +0300

    Исправления

--- BugPro/Program.cs
+++ BugPro/Program.cs
@@ -1,158 +1,108 @@
-// BugPro/Program.cs
+﻿using System;
 using Stateless;
 
-namespace BugPro;
-
-public class Bug
+namespace BugPro
 {
-    public enum State
+    public enum BugState
     {
-        NewDefect,
+        New,
         Triage,
-        NeedMoreInfo,
-        NoTimeNow,
-        SeparateSolution,
-        ProblemAnotherProduct,
-        Fixing,
-        ProblemResolution,
-        TestingByTester,
-        TestingByProductTeam,
-        TestingByDeveloper,
-        Closed,
-        Reopened,
-        NotADefect,
-        DontFix,
+        NeedInfo,
+        InProgress,
+        NotABug,
         Duplicate,
-        NonReproducible
+        CannotReproduce,
+        Fixed,
+        Closed,
+        Reopened
     }
 
-    public enum Trigger
+    public enum BugTrigger
     {
         StartTriage,
         NeedMoreInfo,
-        Ok,
-        NoTimeNow,
-        SeparateSolution,
-        ProblemAnotherProduct,
-        StartFix,
-        ProblemResolved,
-        TestByTester,
-        TestByProductTeam,
-        TestByDeveloper,
+        AssignToDev,
+        MarkNotABug,
+        MarkDuplicate,
+        MarkCannotReproduce,
+        Fix,
+        Verify,
         Close,
         Reopen,
-        NotADefect,
-        DontFix,
-        Duplicate,
-        NonReproducible
+        ReturnToTriage
     }
 
-    private StateMachine<State, Trigger> _machine;
-
-    public Bug()
+    public class Bug
     {
-        _machine = new StateMachine<State, Trigger>(State.NewDefect);
+        private readonly StateMachine<BugState, BugTrigger> _stateMachine;
 
-        _machine.Configure(State.NewDefect)
-            .Permit(Trigger.StartTriage, State.Triage);
+        public BugState State => _stateMachine.State;
 
-        _machine.Configure(State.Triage)
-            .Permit(Trigger.NeedMoreInfo, State.NeedMoreInfo)
-            .Permit(Trigger.NoTimeNow, State.NoTimeNow)
-            .Permit(Trigger.SeparateSolution, State.SeparateSolution)
-            .Permit(Trigger.ProblemAnotherProduct, State.ProblemAnotherProduct)
-            .Permit(Trigger.StartFix, State.Fixing)
-            .Permit(Trigger.NotADefect, State.NotADefect)
-            .Permit(Trigger.DontFix, State.DontFix)
-            .Permit(Trigger.Duplicate, State.Duplicate)
-            .Permit(Trigger.NonReproducible, State.NonReproducible);
+        public Bug()
+        {
+            _stateMachine = new StateMachine<BugState, BugTrigger>(BugState.New);
+            SetupTransitions();
+        }
 
-        _machine.Configure(State.NeedMoreInfo)
-            .Permit(Trigger.Ok, State.Triage);
+        private void SetupTransitions()
+        {
+            _stateMachine.Configure(BugState.New)
+                .Permit(BugTrigger.StartTriage, BugState.Triage);
 
-        _machine.Configure(State.NoTimeNow)
-            .Permit(Trigger.Ok, State.Triage);
+            _stateMachine.Configure(BugState.Triage)
+                .Permit(BugTrigger.NeedMoreInfo, BugState.NeedInfo)
+                .Permit(BugTrigger.AssignToDev, BugState.InProgress)
+                .Permit(BugTrigger.MarkNotABug, BugState.NotABug)
+                .Permit(BugTrigger.MarkDuplicate, BugState.Duplicate)
+                .Permit(BugTrigger.MarkCannotReproduce, BugState.CannotReproduce);
 
-        _machine.Configure(State.SeparateSolution)
-            .Permit(Trigger.Ok, State.Triage);
+            _stateMachine.Configure(BugState.NeedInfo)
+                .Permit(BugTrigger.ReturnToTriage, BugState.Triage);
 
-        _machine.Configure(State.ProblemAnotherProduct)
-            .Permit(Trigger.Ok, State.Triage);
+            _stateMachine.Configure(BugState.InProgress)
+                .Permit(BugTrigger.Fix, BugState.Fixed);
 
-        _machine.Configure(State.NotADefect)
-            .Permit(Trigger.Ok, State.Closed);
+            _stateMachine.Configure(BugState.Fixed)
+                .Permit(BugTrigger.Verify, BugState.Closed)
+                .Permit(BugTrigger.Reopen, BugState.Reopened);
 
-        _machine.Configure(State.DontFix)
-            .Permit(Trigger.Ok, State.Closed);
+            _stateMachine.Configure(BugState.Reopened)
+                .Permit(BugTrigger.AssignToDev, BugState.InProgress);
 
-        _machine.Configure(State.Duplicate)
-            .Permit(Trigger.Ok, State.Closed);
+            _stateMachine.Configure(BugState.NotABug)
+                .Permit(BugTrigger.Close, BugState.Closed);
 
-        _machine.Configure(State.NonReproducible)
-            .Permit(Trigger.Ok, State.Closed);
+            _stateMachine.Configure(BugState.Duplicate)
+                .Permit(BugTrigger.Close, BugState.Closed);
 
-        _machine.Configure(State.Fixing)
-            .Permit(Trigger.ProblemResolved, State.ProblemResolution)
-            .Permit(Trigger.TestByTester, State.TestingByTester)
-            .Permit(Trigger.TestByProductTeam, State.TestingByProductTeam)
-            .Permit(Trigger.TestByDeveloper, State.TestingByDeveloper);
+            _stateMachine.Configure(BugState.CannotReproduce)
+                .Permit(BugTrigger.Close, BugState.Closed);
+        }
 
-        _machine.Configure(State.ProblemResolution)
-            .Permit(Trigger.Ok, State.Triage);
-
-        _machine.Configure(State.TestingByTester)
-            .Permit(Trigger.Close, State.Closed)
-            .Permit(Trigger.Reopen, State.Reopened);
-
-        _machine.Configure(State.TestingByProductTeam)
-            .Permit(Trigger.Close, State.Closed)
-            .Permit(Trigger.Reopen, State.Reopened);
-
-        _machine.Configure(State.TestingByDeveloper)
-            .Permit(Trigger.Close, State.Closed)
-            .Permit(Trigger.Reopen, State.Reopened);
-
-        _machine.Configure(State.Closed)
-            .Permit(Trigger.Reopen, State.Reopened);
-
-        _machine.Configure(State.Reopened)
-            .Permit(Trigger.StartFix, State.Fixing);
+        public void Fire(BugTrigger trigger)
+        {
+            _stateMachine.Fire(trigger);
+        }
     }
 
-    public State CurrentState => _machine.State;
-
-    public void StartTriage() => _machine.Fire(Trigger.StartTriage);
-    public void NeedMoreInfo() => _machine.Fire(Trigger.NeedMoreInfo);
-    public void Ok() => _machine.Fire(Trigger.Ok);
-    public void NoTimeNow() => _machine.Fire(Trigger.NoTimeNow);
-    public void SeparateSolution() => _machine.Fire(Trigger.SeparateSolution);
-    public void ProblemAnotherProduct() => _machine.Fire(Trigger.ProblemAnotherProduct);
-    public void StartFix() => _machine.Fire(Trigger.StartFix);
-    public void ProblemResolved() => _machine.Fire(Trigger.ProblemResolved);
-    public void TestByTester() => _machine.Fire(Trigger.TestByTester);
-    public void TestByProductTeam() => _machine.Fire(Trigger.TestByProductTeam);
-    public void TestByDeveloper() => _machine.Fire(Trigger.TestByDeveloper);
-    public void Close() => _machine.Fire(Trigger.Close);
-    public void Reopen() => _machine.Fire(Trigger.Reopen);
-    public void NotADefect() => _machine.Fire(Trigger.NotADefect);
-    public void DontFix() => _machine.Fire(Trigger.DontFix);
-    public void Duplicate() => _machine.Fire(Trigger.Duplicate);
-    public void NonReproducible() => _machine.Fire(Trigger.NonReproducible);
-}
-
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        Bug bug = new Bug();
-        Console.WriteLine($"Текущее состояние: {bug.CurrentState}");
-        bug.StartTriage();
-        Console.WriteLine($"После StartTriage: {bug.CurrentState}");
-        bug.StartFix();
-        Console.WriteLine($"После StartFix: {bug.CurrentState}");
-        bug.TestByTester();
-        Console.WriteLine($"После TestByTester: {bug.CurrentState}");
-        bug.Close();
-        Console.WriteLine($"После Close: {bug.CurrentState}");
+        static void Main()
+        {
+            var bug = new Bug();
+            Console.WriteLine($"Current: {bug.State}");
+
+            bug.Fire(BugTrigger.StartTriage);
+            Console.WriteLine($"After triage: {bug.State}");
+
+            bug.Fire(BugTrigger.AssignToDev);
+            Console.WriteLine($"Assigned: {bug.State}");
+
+            bug.Fire(BugTrigger.Fix);
+            Console.WriteLine($"Fixed: {bug.State}");
+
+            bug.Fire(BugTrigger.Verify);
+            Console.WriteLine($"Closed: {bug.State}");
+        }
     }
 }
---
+++ BugTests/UnitTest1.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BugPro;
+
+namespace BugTests
+{
+    [TestClass]
+    public class BugWorkflowTests
+    {
+        private Bug _bug;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _bug = new Bug();
+        }
+
+        [TestMethod]
+        public void NewBug_ShouldStartInCreatedState()
+        {
+            Assert.IsTrue(_bug.State == BugState.New, "Initial state must be New");
+        }
+
+        [TestMethod]
+        public void Triage_ShouldBeReachableFromNew()
+        {
+            _bug.Fire(BugTrigger.StartTriage);
+            Assert.AreEqual(BugState.Triage, _bug.State, "After StartTriage state should be Triage");
+        }
+
+        [TestMethod]
+        public void NeedInfo_ShouldTransitionFromTriage()
+        {
+            _bug.Fire(BugTrigger.StartTriage);
+            _bug.Fire(BugTrigger.NeedMoreInfo);
+            Assert.AreEqual(BugState.NeedInfo, _bug.State);
+        }
+
+        [TestMethod]
+        public void NeedInfo_CanGoBackToTriage()
+        {
+            _bug.Fire(BugTrigger.StartTriage);
+            _bug.Fire(BugTrigger.NeedMoreInfo);
+            _bug.Fire(BugTrigger.ReturnToTriage);
+            Assert.AreEqual(BugState.Triage, _bug.State);
+        }
+
+        [TestMethod]
+        public void AssignToDev_MovesToInProgress()
+        {
+            _bug.Fire(BugTrigger.StartTriage);
+            _bug.Fire(BugTrigger.AssignToDev);
+            Assert.AreEqual(BugState.InProgress, _bug.State);
+        }
+
+        [TestMethod]
+        public void Fix_ChangesStateFromInProgressToFixed()
+        {
+            _bug.Fire(BugTrigger.StartTriage);
+            _bug.Fire(BugTrigger.AssignToDev);
+            _bug.Fire(BugTrigger.Fix);
+            Assert.AreEqual(BugState.Fixed, _bug.State);
+        }
+
+        [TestMethod]
+        public void Verify_ClosesFixedBug()
+        {
+            _bug.Fire(BugTrigger.StartTriage);
+            _bug.Fire(BugTrigger.AssignToDev);
+            _bug.Fire(BugTrigger.Fix);
+            _bug.Fire(BugTrigger.Verify);
+            Assert.AreEqual(BugState.Closed, _bug.State);
+        }
+
+        [TestMethod]
+        public void Reopen_ReturnsToReopenedState()
+        {
+            _bug.Fire(BugTrigger.StartTriage);
+            _bug.Fire(BugTrigger.AssignToDev);
+            _bug.Fire(BugTrigger.Fix);
+            _bug.Fire(BugTrigger.Reopen);
+            Assert.AreEqual(BugState.Reopened, _bug.State);
+        }
+
+        [TestMethod]
+        public void ReopenedBug_CanBeAssignedAgain()
+        {
+            _bug.Fire(BugTrigger.StartTriage);
+            _bug.Fire(BugTrigger.AssignToDev);
+            _bug.Fire(BugTrigger.Fix);
+            _bug.Fire(BugTrigger.Reopen);
+            _bug.Fire(BugTrigger.AssignToDev);
+            Assert.AreEqual(BugState.InProgress, _bug.State);
+        }
+
+        [TestMethod]
+        public void Triage_MarkingNotABug_LeadsToNotABugState()
+        {
+            _bug.Fire(BugTrigger.StartTriage);
+            _bug.Fire(BugTrigger.MarkNotABug);
+            Assert.AreEqual(BugState.NotABug, _bug.State);
+        }
+
+        [TestMethod]
+        public void NotABug_CanBeClosed()
+        {
+            _bug.Fire(BugTrigger.StartTriage);
+            _bug.Fire(BugTrigger.MarkNotABug);
+            _bug.Fire(BugTrigger.Close);
+            Assert.AreEqual(BugState.Closed, _bug.State);
+        }
+
+        [TestMethod]
+        public void Duplicate_MarkingWorks()
+        {
+            _bug.Fire(BugTrigger.StartTriage);
+            _bug.Fire(BugTrigger.MarkDuplicate);
+            Assert.AreEqual(BugState.Duplicate, _bug.State);
+        }
+
+        [TestMethod]
+        public void CannotReproduce_StateReachable()
+        {
+            _bug.Fire(BugTrigger.StartTriage);
+            _bug.Fire(BugTrigger.MarkCannotReproduce);
+            Assert.AreEqual(BugState.CannotReproduce, _bug.State);
+        }
+    }
+}
